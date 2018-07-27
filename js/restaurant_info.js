4683eee8677e087c983fd6c663da4c5a8d42b17b
Michele De Palma <35871747+Dev-Mikko@users.noreply.github.com>
2018-07-27 17:40:40 +0200

    API problem fixing
    Insert a forgotten API key

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiZGV2bWlra28iLCJhIjoiY2pqdHMxdnlvMjltaTNrbGVkZ2dqNzE4eSJ9.9jwwiPCZJ06whawGNlTyww',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
